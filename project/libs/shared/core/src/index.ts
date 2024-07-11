4e6668dd1b299133ca4b2f165a39e7d07c887e8c
SJ23y <mcfolland@gmail.com>
2024-07-11 03:26:37 +0300

    Созданы интерфейсы для сущности User

---
+++ project/libs/shared/core/src/index.ts
@@ -0,0 +1,3 @@
+export { User } from './lib/types/user.interface';
+export { AuthUser } from './lib/types/auth-user.interface';
+export { Entity } from './lib/base/entity';
---
+++ project/libs/shared/core/src/lib/base/entity.ts
@@ -0,0 +1,11 @@
+export class Entity {
+  private _id = '';
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set id(value: string) {
+    this._id = value;
+  }
+}
